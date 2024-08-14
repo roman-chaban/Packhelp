dae293d707a27a70bf43e2f09498fc82eaa878a7
Roman Chaban <roman.chaban1001@gmail.com>
2024-08-13 23:25:59 -0800

    Added showing content for headerNav & productCard titles

---
+++ scripts/header.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const fetchHeaderLinks = async () => {
+  try {
+    const request = await fetch('../json/header.json');
+    const data = await request.json();
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+  } finally {
+    console.log('Links is fetching');
+  }
+};
+
+const setHeaderLinksByNavMenu = (data) => {
+  if (!Array.isArray(data)) {
+    console.error('Expected data to be an array');
+    return '';
+  }
+
+  const menuItems = data
+    .map((link) => {
+      return `<li class='menuListItem'><a class="menuListLink" href="#!">
+        ${link.linkItem}
+      </a>
+      </li>
+      `;
+    })
+    .join('');
+
+  const menu = document.createElement('ul');
+  menu.classList.add('headerNavMenu');
+  menu.innerHTML = menuItems;
+
+  return menu.outerHTML;
+};
+
+fetchHeaderLinks()
+  .then((data) => {
+    const headerLinksHTML = setHeaderLinksByNavMenu(data);
+    document.querySelector('.headerNav').innerHTML = headerLinksHTML;
+  })
+  .catch((error) => {
+    console.error('Error fetching header links:', error);
+  });
---
+++ scripts/products.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const productCardsContainer = document.querySelector('.productsCards');
+
+const fetchingProductsTitles = async () => {
+  try {
+    const request = await fetch('../json/products.json');
+    const data = await request.json();
+    return data;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const setTitlesByProducts = (data) => {
+  if (!Array.isArray(data)) {
+    console.error('Titles is not defined, Sorry...');
+  }
+  const productCards = [...document.querySelectorAll('.productCard')];
+  productCards.forEach((card, index) => {
+    if (index < data.length) {
+      const titleElement = document.createElement('h4');
+      titleElement.className = 'cardTitle';
+      titleElement.textContent = data[index].productTitle;
+
+      const cardImageBlock = card.querySelector('.cardImageBlock');
+      if (cardImageBlock) {
+        cardImageBlock.insertAdjacentElement('afterend', titleElement);
+      }
+    }
+  });
+};
+
+fetchingProductsTitles().then((titles) => {
+  setTitlesByProducts(titles);
+});
